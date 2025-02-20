4489b00f020686de40631a3b74f0ad254e6340e4
santiicp <tisan11ca@gmail.com>
2025-02-20 09:54:19 +0100

    Initial commit

---
+++ src/main/java/com/example/apiwistonspring/ApiWistonSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiwistonspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWistonSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiWistonSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apiwistonspring/ApiWistonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiwistonspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWistonSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

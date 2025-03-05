37fe7df65dba075154b8cfa4c40e073aa3408bec
santiicp <tisan11ca@gmail.com>
2025-03-05 18:13:57 +0100

    Arreglando los repositorios

--- src/main/java/com/example/apiwistonspring/model/repositories/MarcaRepository.java
+++ src/main/java/com/example/apiwistonspring/model/repositories/MarcaRepository.java
@@ -2,6 +2,8 @@ package com.example.apiwistonspring.model.repositories;
 
 import com.example.apiwistonspring.model.entities.Marca;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MarcaRepository extends JpaRepository<Marca, Long> {
 }
--- src/main/java/com/example/apiwistonspring/model/repositories/PantallaRepository.java
+++ src/main/java/com/example/apiwistonspring/model/repositories/PantallaRepository.java
@@ -1,6 +1,8 @@
 package com.example.apiwistonspring.model.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PantallaRepository extends JpaRepository<Modelo, Long> {
 }

21660966ad04c6c72cb7d4319d79ad201f77031f
raquelrza <ramirezariasraquel@gmail.com>
2025-03-09 19:11:06 +0100

    cambios en FilterTest ok

--- src/test/java/com/example/apiwistonspring/filters/FilterTest.java
+++ src/test/java/com/example/apiwistonspring/filters/FilterTest.java
@@ -36,31 +36,60 @@ class FilterTest {
 	@Test
 	void filterMovilesByAlmacenamiento() {
 		// Creamos el filtro que busca por almacenamiento
-		FilterDTO filterDTO = new FilterDTO(100.00, 400.00,128, null, null, null, null, null, null, null, null, null);
+		FilterDTO filterDTO = new FilterDTO(0.10, 1000.00, 128, null, null, null, null, null, null, null, null, null);
 		List<Movil> filtrados = movilService.filterMoviles(filterDTO);
-		assertEquals(2, filtrados.size());
+		assertEquals(3, filtrados.size());
 
 	}
 
 	@Test
 	void filterMovilesByMarca() {
-		FilterDTO filterDTO = new FilterDTO(100.00, 400.00,null, "Apple", null, null, null, null, null, null, null, null);
+		FilterDTO filterDTO = new FilterDTO(10.00, 900.00, null, "Apple", null, null, null, null, null, null, null,
+				null);
 		List<Movil> filtradoMarca = movilService.filterMoviles(filterDTO);
-		assertEquals(1, filtradoMarca.size());
+		assertEquals(3, filtradoMarca.size());
+
+		// "apple" y almacenamiento 64
+		FilterDTO filterDTO2 = new FilterDTO(10.00, 900.00, 64, "Apple", null, null, null, null, null, null, null,
+				null);
+		List<Movil> filtradoMarca2 = movilService.filterMoviles(filterDTO2);
+		assertEquals(2, filtradoMarca2.size());
 
 	}
 
 	@Test
 	void filterMovilesMultipleFilters() {
-		FilterDTO filterDTO = new FilterDTO(100.00, 400.00,128, "Samsung", null, null, null, null, null, null, null, null);
+		FilterDTO filterDTO = new FilterDTO(10.00, 900.00, 128, "Samsung", null, true, null, null, null, null, null,
+				null);
 		List<Movil> filtradoMulti = movilService.filterMoviles(filterDTO);
 		assertEquals(2, filtradoMulti.size());
+
+		// probando también nfc
+		FilterDTO filterDTO2 = new FilterDTO(100.00, 800.00, 128, "samsung", null, true, null, null, null, null, null,
+				null);
+		List<Movil> filtradoMulti2 = movilService.filterMoviles(filterDTO2);
+		assertEquals(1, filtradoMulti2.size());
+
+		FilterDTO filterDTO3 = new FilterDTO(10.00, 50.00, 128, "Samsung", null, true, null, null, null, null, null,
+				null);
+		List<Movil> filtradoMulti3 = movilService.filterMoviles(filterDTO3);
+		assertEquals(0, filtradoMulti3.size());
+
 	}
-	
+
 	@Test
 	void filtrarPorPrecio() {
 		FilterDTO filterDTO = new FilterDTO(100.00, 400.00, null, null, null, null, null, null, null, null, null, null);
 		List<Movil> filtradoprecio = movilService.filterMoviles(filterDTO);
 		assertEquals(2, filtradoprecio.size());
+	}
+
+	@Test
+	void probandoMasFiltros() {
+		//probando todos los filtros a la vez 
+		FilterDTO filter = new FilterDTO(10.0, 900.0, 128, "Samsung", 108, true, 6.5, "Snapdragon 888", 90, 1, 9,
+				"AMOLED");
+		List<Movil> filtraList = movilService.filterMoviles(filter);
+		assertEquals(1, filtraList.size());
 	}
 }

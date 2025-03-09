aad239eed09582c531c5fea4e601c2251fd30730
santiicp <tisan11ca@gmail.com>
2025-03-09 10:48:36 +0100

    Terminando comparar dos moviles

--- src/main/java/com/example/apiwistonspring/controllers/MovilComparisionController.java
+++ src/main/java/com/example/apiwistonspring/controllers/MovilComparisionController.java
@@ -4,6 +4,7 @@ import com.example.apiwistonspring.dtos.MovilComparisionDTO;
 import com.example.apiwistonspring.services.ComparisionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ public class MovilComparisionController {
         this.comparisionService = comparisionService;
     }
 
+    @GetMapping("/compare")
     public ResponseEntity<List<MovilComparisionDTO>> compareTwoMoviles(@RequestParam Long idMovil1,@RequestParam Long idMovil2) {
         return ResponseEntity.ok(comparisionService.compareTwoMoviles(idMovil1, idMovil2));
     }

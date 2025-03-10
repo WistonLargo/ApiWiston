b85be29e52e7d076d86c33f1351d93218506085f
santiicp <tisan11ca@gmail.com>
2025-03-10 08:32:49 +0100

    Arreglando el test

--- src/test/java/com/example/apiwistonspring/services/ComparisionServiceTest.java
+++ src/test/java/com/example/apiwistonspring/services/ComparisionServiceTest.java
@@ -2,6 +2,7 @@ package com.example.apiwistonspring.services;
 
 import com.example.apiwistonspring.dtos.MovilComparisionDTO;
 import com.example.apiwistonspring.model.entities.*;
+import com.example.apiwistonspring.model.repositories.MovilRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDate;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
@@ -17,19 +19,12 @@ class ComparisionServiceTest {
     @Autowired
     ComparisionServiceImpl comparisionService;
 
-
     @Test
     void compareTwoMoviles() {
 
-
-        Movil movil1 = new Movil(128, new Dimensiones(), LocalDate.now(), 48, true, 230.50, 200.99, 5, 8,68, new Modelo(), new Pantalla(), Long.valueOf(123456L), new Procesador());
-        Movil movil2 = new Movil(256, new Dimensiones(), LocalDate.now(), 64, false, 180.75, 299.99, 8, 12,29, new Modelo(), new Pantalla(), Long.valueOf(654321L), new Procesador());
-
         List<MovilComparisionDTO> movils = comparisionService.compareTwoMoviles(1L, 2L);
 
-        System.out.println(movil1);
-        System.out.println(movil2);
-        System.out.println(movils);
+        assertEquals(2, movils.size());
 
 
     }

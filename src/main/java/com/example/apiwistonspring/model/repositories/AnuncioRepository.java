cdcb67419d863d88776b3d80aa6a482b83121cd1
raquelrza <ramirezariasraquel@gmail.com>
2025-03-12 18:25:43 +0100

    deletingold

--- src/main/java/com/example/apiwistonspring/model/repositories/AnuncioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.apiwistonspring.model.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.apiwistonspring.model.entities.OldAnuncio;
-
-public interface AnuncioRepository extends JpaRepository<OldAnuncio, Long> {
-
-}
